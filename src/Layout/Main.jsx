db89b2f93abc0dca3339bb97c311139b19db1bf4
Mosharrof Hosen Munna <mdmunna31573@gmail.com>
2022-11-08 13:15:35 +0600

    create navbar and setup react router for routing

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Outlet } from 'react-router'
+import Footer from '../Pages/Shared/Footer/Footer'
+import Navigation from '../Pages/Shared/Navigation/Navigation'
+
+const Main = () => {
+  return (
+    <div>
+        <Navigation/>
+        <Outlet/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Main
